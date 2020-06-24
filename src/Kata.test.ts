fea1046781252c7f58d8083dd5fc44e5b26a6f85
Ichizo Umehara <iumehara10@gmail.com>
2020-06-24 13:55:35 +0900

    all rps tests

--- src/Kata.test.ts
+++ src/Kata.test.ts
@@ -3,6 +3,7 @@ import {Kata, Observer} from './Kata'
 class SpyObserver implements Observer {
   p1Wins_wasCalled = false
   p2Wins_wasCalled = false
+  draw_wasCalled = false
 
   p1Wins(): void {
     this.p1Wins_wasCalled = true
@@ -11,9 +12,13 @@ class SpyObserver implements Observer {
   p2Wins(): void {
     this.p2Wins_wasCalled = true
   }
+
+  draw(): void {
+    this.draw_wasCalled = true
+  }
 }
 
-describe('1st test suite', () => {
+describe('Kata', () => {
   test('rock vs scissors', () => {
     const observer = new SpyObserver()
     new Kata().someMethod('rock', 'scissors', observer)
@@ -28,5 +33,72 @@ describe('1st test suite', () => {
 
     expect(observer.p2Wins_wasCalled).toBe(true)
     expect(observer.p1Wins_wasCalled).toBe(false)
+  })
+
+  test('scissors vs paper', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('scissors', 'paper', observer)
+
+    expect(observer.p1Wins_wasCalled).toBe(true)
+    expect(observer.p2Wins_wasCalled).toBe(false)
+  })
+
+  test('scissors vs paper', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('scissors', 'paper', observer)
+
+    expect(observer.p1Wins_wasCalled).toBe(true)
+    expect(observer.p2Wins_wasCalled).toBe(false)
+  })
+
+  test('paper vs scissors', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('paper', 'scissors', observer)
+
+    expect(observer.p2Wins_wasCalled).toBe(true)
+    expect(observer.p1Wins_wasCalled).toBe(false)
+  })
+
+  test('paper vs rock', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('paper', 'rock', observer)
+
+    expect(observer.p1Wins_wasCalled).toBe(true)
+    expect(observer.p2Wins_wasCalled).toBe(false)
+  })
+
+  test('rock vs paper', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('rock', 'paper', observer)
+
+    expect(observer.p2Wins_wasCalled).toBe(true)
+    expect(observer.p1Wins_wasCalled).toBe(false)
+  })
+
+  test('rock vs rock', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('rock', 'rock', observer)
+
+    expect(observer.draw_wasCalled).toBe(true)
+    expect(observer.p1Wins_wasCalled).toBe(false)
+    expect(observer.p2Wins_wasCalled).toBe(false)
+  })
+
+  test('paper vs paper', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('paper', 'paper', observer)
+
+    expect(observer.draw_wasCalled).toBe(true)
+    expect(observer.p1Wins_wasCalled).toBe(false)
+    expect(observer.p2Wins_wasCalled).toBe(false)
+  })
+
+  test('scissors vs scissors', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('scissors', 'scissors', observer)
+
+    expect(observer.draw_wasCalled).toBe(true)
+    expect(observer.p1Wins_wasCalled).toBe(false)
+    expect(observer.p2Wins_wasCalled).toBe(false)
   })
 })
