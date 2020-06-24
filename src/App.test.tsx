5424bd3241f906738d6b4af6d21cec64e77cf531
Summer Li <summerli@vmware.com>
2020-06-24 16:46:36 +0900

    React Sample Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('show JankenGame Title', () => {
+  const { queryByText } = render(<App />);
+  const titleElement = queryByText('Janken Game');
+
+  expect(titleElement).toBeInTheDocument();
 });
