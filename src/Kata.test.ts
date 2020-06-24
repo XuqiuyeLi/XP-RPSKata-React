4fb92e6733bde635039ae0dd3edfb7b5b9468934
Ichizo Umehara <iumehara10@gmail.com>
2020-06-24 12:33:53 +0900

    rvs, svr

--- src/Kata.test.ts
+++ src/Kata.test.ts
@@ -1,9 +1,32 @@
-import {Kata} from './Kata'
+import {Kata, Observer} from './Kata'
+
+class SpyObserver implements Observer {
+  p1Wins_wasCalled = false
+  p2Wins_wasCalled = false
+
+  p1Wins(): void {
+    this.p1Wins_wasCalled = true
+  }
+
+  p2Wins(): void {
+    this.p2Wins_wasCalled = true
+  }
+}
 
 describe('1st test suite', () => {
-    test('rock vs scissors', () => {
-        const result = new Kata().someMethod('rock', 'scissors')
+  test('rock vs scissors', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('rock', 'scissors', observer)
 
-        expect(result).toBe('Player 1 Wins!')
-    })
+    expect(observer.p1Wins_wasCalled).toBe(true)
+    expect(observer.p2Wins_wasCalled).toBe(false)
+  })
+
+  test('scissors vs rock', () => {
+    const observer = new SpyObserver()
+    new Kata().someMethod('scissors', 'rock', observer)
+
+    expect(observer.p2Wins_wasCalled).toBe(true)
+    expect(observer.p1Wins_wasCalled).toBe(false)
+  })
 })
--- src/Kata.ts
+++ src/Kata.ts
@@ -1,5 +1,15 @@
+export interface Observer {
+  p1Wins(): void
+
+  p2Wins(): void
+}
+
 export class Kata {
-    someMethod(player1: string, player2: string) {
-        return 'Player 1 Wins!'
+  someMethod(player1: string, player2: string, observer: Observer) {
+    if (player1 === 'rock') {
+      observer.p1Wins()
+    } else if (player1 === 'scissors') {
+      observer.p2Wins()
     }
+  }
 }
